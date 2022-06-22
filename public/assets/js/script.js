d445f4f37aab8ce2d5486f578103e255a3b92c9d
FERRARIOloic <ferrarioloic@gmail.com>
2022-06-22 11:21:33 +0200

    commit

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,4 +1,21 @@
 
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar").classList.add('navbarScrollShow');
+        document.getElementById("navbar").classList.remove('navbarScrollHide');
+    } else {
+        document.getElementById("navbar").classList.add('navbarScrollHide');
+        document.getElementById("navbar").classList.remove('navbarScrollShow');
+        document.getElementById("navbarSupportedContent").classList.remove('show');
+    }
+    prevScrollpos = currentScrollPos;
+}
+
+
+
+
 let btnChoose = document.querySelectorAll('#btnChoose');
 console.log(btnChoose);
 
@@ -168,6 +185,7 @@ btnChoose.forEach(element => {
         }
 
     })
+
 
 }
 )
