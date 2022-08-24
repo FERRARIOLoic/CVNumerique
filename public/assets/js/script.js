5a530507c615e66ac57cce5bf9450f2c88444ce9
FERRARIOloic <ferrarioloic@gmail.com>
2022-08-24 19:39:13 +0200

    cv2

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -4,10 +4,12 @@ window.onscroll = function () {
     var currentScrollPos = window.pageYOffset;
     if (prevScrollpos > currentScrollPos) {
         document.getElementById("navbar").classList.add('navbarScrollShow');
+        document.getElementById("navbar").classList.add('navbarAppear');
         document.getElementById("navbar").classList.remove('navbarScrollHide');
     } else {
         document.getElementById("navbar").classList.add('navbarScrollHide');
         document.getElementById("navbar").classList.remove('navbarScrollShow');
+        document.getElementById("navbar").classList.remove('navbarAppear');
         document.getElementById("navbarSupportedContent").classList.remove('show');
     }
     prevScrollpos = currentScrollPos;
