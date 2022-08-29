38212f671d6fe2415e137c13034b59e3c4805862
FERRARIOloic <ferrarioloic@gmail.com>
2022-08-29 13:08:42 +0200

    design parcours

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -14,16 +14,16 @@ window.onscroll = function () {
 }
 
 
-function isInViewport(el) {
-    const rect = el.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+// function isInViewport(el) {
+//     const rect = el.getBoundingClientRect();
+//     return (
+//         rect.top >= 0 &&
+//         rect.left >= 0 &&
+//         rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+//         rect.right <= (window.innerWidth || document.documentElement.clientWidth)
 
-    );
-}
+//     );
+// }
 
 const boxPresentation = document.querySelector('#boxPresentation');
 document.addEventListener('scroll', function () {
